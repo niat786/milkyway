8f032e6de2a14dcca84d85eba59ec894ea592c03
niat786 <niat786@gmail.com>
2024-08-12 12:11:53 +0500

    download page added

--- app/page.js
+++ app/page.js
@@ -24,27 +24,24 @@ export default function Home() {
               the endless promotions, great looks and engaging gameplay, now
               improved for the even more excitement.
             </p>
-            <p className="font-semibold tracking-wider text-center">
+            <p className="font-bold text-transparent bg-clip-text bg-gradient-to-tr from-blue-500 to-blue-900 tracking-wider text-center py-5">
               Download for IOS and Android, also you can play games online.
             </p>
-            <div className="flex justify-center py-5 text-center">
-              <a href="#play-online-games">
-                <figure>
-                  <Image
-                    src="/images/milkyway-online.webp"
-                    alt="milkyway online games"
-                    width={500}
-                    height={92}
-                    className="rounded my-4"
-                  />
-                  <figcaption className="text-sm">
-                    Play Milkyway casino games online
-                  </figcaption>
-                </figure>
-              </a>
-            </div>
+
+            <div className="flex flex-col  text-lg space-y-10">
+
+<figure className="mx-auto">
+  <Image src="/milkyway-play-online-games-(screenshot).webp" alt="online-games-screenshot" width={1000} height={565} className="md:rounded-lg rounded" />
+        
+        <figcaption className="text-sm text-center text-gray-700 mt-5">
+          Milkyway Games Online - Screenshot
+        </figcaption>
+      </figure>
+      </div>
+
+      
             <p className="pb-10">
-              The game such as,{" "}
+              The game such as,
               <strong>Skeleton Dance 3D, 5 Deluxe, Kingdom Of Atlantis</strong>
               and many more new in addition to renewed games. Games you love to
               play such as <strong>Hexa Keno and Crab King</strong>.
@@ -89,7 +86,7 @@ export default function Home() {
                       File Size
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                      40.9 MB
+                      50.8 MB
                     </td>
                   </tr>
                   <tr className="flex flex-col md:flex-row justify-between">
@@ -199,15 +196,32 @@ export default function Home() {
             </p>
           </div>
 
+         
+          <div className="flex justify-center py-5 text-center">
+              
+                <figure>
+                  <Image
+                    src="/images/milkyway-online.webp"
+                    alt="milkyway online games - logo"
+                    width={500}
+                    height={92}
+                    className="rounded my-4"
+                  />
+                  <figcaption className="text-sm">
+                    Milky Way App Icon
+                  </figcaption>
+                </figure>
+          
+            </div>
+
           {/* <!-- slider --> */}
 
           {/* <!-- slider ends --> */}
 
           <div className="md:p-5 pt-5 w-full" id="play-online-games">
             <a
-              href="http://play.milkywayapp.xyz/game/milkyway/"
-              rel="external nofollow"
-              target="_blank"
+              href="/app-download"
+             
               className="md:w-[29rem] w-full mx-auto h-16 text-[18px] rounded  bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg shadow-blue-700/30 hover:scale-105 duration-500 text-white flex justify-center items-center"
             >
               <svg
@@ -222,7 +236,7 @@ export default function Home() {
                   clipRule="evenodd"
                 />
               </svg>
-              &nbsp; Play Online&nbsp;{" "}
+              &nbsp; Play Online&nbsp;
             </a>
           </div>
           <div className="md:p-5 pt-5 flex justify-center space-x-2 md:space-x-10">
@@ -238,7 +252,7 @@ export default function Home() {
               >
                 <path d="M420.6 301.9a24 24 0 1 1 24-24 24 24 0 0 1 -24 24m-265.1 0a24 24 0 1 1 24-24 24 24 0 0 1 -24 24m273.7-144.5 47.9-83a10 10 0 1 0 -17.3-10h0l-48.5 84.1a301.3 301.3 0 0 0 -246.6 0L116.2 64.5a10 10 0 1 0 -17.3 10h0l47.9 83C64.5 202.2 8.2 285.6 0 384H576c-8.2-98.5-64.5-181.8-146.9-226.6" />
               </svg>
-              &nbsp; Android&nbsp;{" "}
+              &nbsp; Android&nbsp;
             </a>
             <a
               href="#download-milkyway-app-android-ios"
@@ -252,7 +266,7 @@ export default function Home() {
               >
                 <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
               </svg>
-              &nbsp; IOS&nbsp;{" "}
+              &nbsp; IOS&nbsp;
             </a>
           </div>
         </div>
@@ -261,10 +275,10 @@ export default function Home() {
           id="best-milkyway-casino-games"
           className="md:p-8 lg:p-10  p-4 border border-dashed rounded-xl my-10 text-center"
         >
-          <h2 className="h2">Best Games</h2>
+         <h2 className="text-2xl md:text-3xl my-5">Best Games</h2>
           <p>This gaming app offers amazing games for you.</p>
           <p>
-            The game such as,{" "}
+            The game such as,
             <strong>Skeleton Dance 3D, 5 Deluxe, Kingdom Of Atlantis</strong>
             and many more new in addition to renewed games. games you love to
             play such as <strong> Hexa Keno and Crab King</strong>.
@@ -471,9 +485,8 @@ export default function Home() {
 
           <div className="md:p-5 pt-5 w-full" id="play-online-games">
             <a
-              href="https://milkywayapp.xyz/"
-              rel="external nofollow"
-              target="_blank"
+              href="/app-download"
+             
               className="md:w-[29rem] w-full mx-auto h-16 text-[18px] rounded  bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg shadow-blue-700/30 hover:scale-105 duration-500 text-white flex justify-center items-center"
             >
               <svg
---
+++ pages/app-download.tsx
@@ -0,0 +1,187 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import "../public/css/style.css";
+import Head from "next/head";
+import Image from "next/image";
+
+const MilkywayApk = () => {
+  return (
+    <>
+      <Head>
+        <title>Milkyway App Download - Online Gaming Free </title>
+        <meta
+          name="description"
+          content="Milkyway app download for free on Android/IOS to play games. Get the latest version V3"
+        />
+        <meta
+          property="og:title"
+          content="Milkyway app download - Milkyway Online"
+        />
+        <meta
+          property="og:description"
+          content="Milkyway app download for free on Android/IOS to play games. Get the latest version V3"
+        />
+        <meta property="og:image" content="/og-cover.png" />
+        <meta
+          property="og:url"
+          content="https://milkywayonline.net/app-download"
+        />
+      </Head>
+
+      <Header></Header>
+      <section className=" mx-auto lg:max-w-[96rem] p-2 md:px-4 lg:px-20 ">
+        <div className="md:p-8 lg:p-10 p-4 border border-dashed rounded-xl ">
+          <h1 className="text-center text-2xl md:text-3xl my-5 mb-10">
+          Milkyway app download, gaming app for IOS and Android, play games and win!
+          </h1>
+
+          <div className="flex flex-col  text-lg">
+
+          <figure className="mx-auto ">
+            <Image src="/milkyway-play-online-games-(screenshot).webp" alt="online-games-screenshot" width={1000} height={565} className="md:rounded-lg rounded" />
+                  
+                  <figcaption className="text-sm text-center text-gray-700 mt-5">
+                    Milkyway Games Online - Screenshot
+                  </figcaption>
+                </figure>
+                <div className="md:p-5 pt-5 w-full mt-10" id="play-online-games">
+            <a
+              href="http://play.milkywayapp.xyz/game/milkyway/"
+              rel="external nofollow"
+              target="_blank"
+              className="md:w-[29rem] w-full mx-auto h-16 text-[18px] rounded  bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg shadow-blue-700/30 hover:scale-105 duration-500 text-white flex justify-center items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-8"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.371 8.143c5.858-5.857 15.356-5.857 21.213 0a.75.75 0 0 1 0 1.061l-.53.53a.75.75 0 0 1-1.06 0c-4.98-4.979-13.053-4.979-18.032 0a.75.75 0 0 1-1.06 0l-.53-.53a.75.75 0 0 1 0-1.06Zm3.182 3.182c4.1-4.1 10.749-4.1 14.85 0a.75.75 0 0 1 0 1.061l-.53.53a.75.75 0 0 1-1.062 0 8.25 8.25 0 0 0-11.667 0 .75.75 0 0 1-1.06 0l-.53-.53a.75.75 0 0 1 0-1.06Zm3.204 3.182a6 6 0 0 1 8.486 0 .75.75 0 0 1 0 1.061l-.53.53a.75.75 0 0 1-1.061 0 3.75 3.75 0 0 0-5.304 0 .75.75 0 0 1-1.06 0l-.53-.53a.75.75 0 0 1 0-1.06Zm3.182 3.182a1.5 1.5 0 0 1 2.122 0 .75.75 0 0 1 0 1.061l-.53.53a.75.75 0 0 1-1.061 0l-.53-.53a.75.75 0 0 1 0-1.06Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              &nbsp; Play Online&nbsp;
+            </a>
+          </div>
+          <div className="md:p-5 pt-5 flex justify-center space-x-2 md:space-x-10">
+            <a
+              href="https://pack.dragon-plus.com/apk/MilkyWay.apk"
+              className="w-52 h-16 text-[18px] rounded bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg shadow-blue-700/30 hover:scale-105 duration-500 text-white flex justify-center items-center"
+            >
+              <svg
+                className="h-8 w-8"
+                fill="#fff"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+              >
+                <path d="M420.6 301.9a24 24 0 1 1 24-24 24 24 0 0 1 -24 24m-265.1 0a24 24 0 1 1 24-24 24 24 0 0 1 -24 24m273.7-144.5 47.9-83a10 10 0 1 0 -17.3-10h0l-48.5 84.1a301.3 301.3 0 0 0 -246.6 0L116.2 64.5a10 10 0 1 0 -17.3 10h0l47.9 83C64.5 202.2 8.2 285.6 0 384H576c-8.2-98.5-64.5-181.8-146.9-226.6" />
+              </svg>
+              &nbsp; Android&nbsp;
+            </a>
+            <a
+              href="https://ngvmytjl.121mai0014.com/03tvqd"
+        
+              className="w-52 h-16 text-[18px] rounded  bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg shadow-blue-700/30 hover:scale-105 duration-500 text-white flex justify-center items-center"
+            >
+              <svg
+                className="h-8 w-8 "
+                fill="#fff"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+              >
+                <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
+              </svg>
+              &nbsp; IOS&nbsp;
+            </a>
+          </div>
+
+          <p className="my-10">Download the milky way app on ypur mobile phone and,
+              explore hundreds of top online casino games, from slots and table
+              games to live dealer experiences. 
+            </p>
+            <p className="mb-10">
+            Enjoy fantastic bonuses with
+              your deposits and benefit from our regular promotions. Play your
+              favorite casino games anytime, anywhere on your mobile device, all
+              while keeping your data secure with advanced encryption
+              technology.
+            </p>
+          
+            <div className="max-w-xl  mx-auto  rounded-xl overflow-hidden  bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg shadow-sky-700/20">
+              <table className=" w-full divide-y table-fixed divide-gray-100 p-10">
+                <thead className="">
+                  <tr className="flex flex-col md:flex-row justify-between">
+                  <th className="px-6 py-3 text-left text-white font-medium uppercase tracking-wider">
+                      App info
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-100">
+                  <tr className="flex flex-col md:flex-row justify-between">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      Package Name
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
+                      com.ultra.panda.casino
+                    </td>
+                  </tr>
+                  <tr className="flex flex-col md:flex-row justify-between">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      Languages
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
+                      English, 72 more
+                    </td>
+                  </tr>
+                  <tr className="flex flex-col md:flex-row justify-between">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      Requires Android
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
+                      Android 5.0+ (Lollipop, API 21)
+                    </td>
+                  </tr>
+                  <tr className="flex flex-col md:flex-row justify-between">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      Content Rating
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
+                      Teen, Simulated Gambling
+                    </td>
+                  </tr>
+                  <tr className="flex flex-col md:flex-row justify-between">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      Architecture
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
+                      arm64-v8a, armeabi-v7a
+                    </td>
+                  </tr>
+                  <tr className="flex flex-col md:flex-row justify-between">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      Permissions
+                    </td>
+                    <td className="md:px-6 px-2 text-xs py-4 whitespace-nowrap text-gray-700 ">
+                      <p>android.permission.INTERNET</p>
+                      <p>android.permission.ACCESS_NETWORK_STATE</p>
+                      <p>com.google.android.gms.permission.AD_ID</p>
+                      <p>android.permission.FOREGROUND_SERVICE</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+       
+        </div>
+      </section>
+
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default MilkywayApk;
